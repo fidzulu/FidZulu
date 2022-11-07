71519fd685fbca8f176ac5dce8918e8a9f91d3c3
Suicide5quad <okayawesome121@gmail.com>
2022-11-07 16:41:40 +0530

    added routes for toys

--- MidTire1/routes/index.js
+++ MidTire1/routes/index.js
@@ -3,62 +3,96 @@ var request = require("request");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-    res.render("index", { title: "Express" });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.route("/food/team").get(function(req, res) {
-    request({
-            method: "GET",
-            uri: "http://localhost:3032/food/team",
-        },
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                res.send(body);
-            }
-        }
-    );
-});
- 
-router.route("/food/all/:location").get(function(req, res) {
-  const location = req.params.location;
-  request({
-          method: "GET",
-          uri: "http://localhost:3032/food/all/" + location,
-      },
-      function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-              res.send(body);
-          }
+router.route("/food/team").get(function (req, res) {
+  request(
+    {
+      method: "GET",
+      uri: "http://localhost:3032/food/team",
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
       }
+    }
   );
 });
 
-router.route("/bikesTeam").get(function(req, res) {
-    request({
-            method: "GET",
-            uri: "http://localhost:3031/bikes/team",
-        },
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                res.send(body);
-            }
-        }
-    );
+router.route("/food/all/:location").get(function (req, res) {
+  const location = req.params.location;
+  request(
+    {
+      method: "GET",
+      uri: "http://localhost:3032/food/all/" + location,
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }
+    }
+  );
 });
 
-router.route("/bikes/:location").get(function(req, res) {
-    const location = req.params.location;
-    request({
-            method: "GET",
-            uri: "http://localhost:3031/bikes/all/" + location,
-        },
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                res.send(body);
-            }
-        }
-    );
+router.route("/bikes/team").get(function (req, res) {
+  request(
+    {
+      method: "GET",
+      uri: "http://localhost:3031/bikes/team",
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }
+    }
+  );
+});
+
+router.route("/bikes/:location").get(function (req, res) {
+  const location = req.params.location;
+  request(
+    {
+      method: "GET",
+      uri: "http://localhost:3031/bikes/all/" + location,
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }
+    }
+  );
+});
+
+// GET requests for toys
+router.route("/toys/team").get(function (req, res) {
+  request(
+    {
+      method: "GET",
+      uri: "http://localhost:3033/toys/team",
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }
+    }
+  );
+});
+
+router.route("/toys/:location").get(function (req, res) {
+  const location = req.params.location;
+  request(
+    {
+      method: "GET",
+      uri: "http://localhost:3033/toys/all/" + location,
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }
+    }
+  );
 });
 
 module.exports = router;
