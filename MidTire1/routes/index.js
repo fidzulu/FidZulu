ba946128ae76527d3ee5fc03cdb4604ab953e762
MakineediSaiHarsh <msaiharsh@gmail.com>
2022-11-07 14:59:52 +0530

    Simple Test working

--- MidTire1/routes/index.js
+++ MidTire1/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+router.route('/testRoute')
+  .get(function(req, res){
+    request({
+      method: 'GET',
+      uri: 'http://localhost:3032/food/all/Durham'
+    }, function (error, response, body){
+      if(!error && response.statusCode == 200){
+        res.json(body);
+      }
+    })
+  });
+
+
 
 module.exports = router;
