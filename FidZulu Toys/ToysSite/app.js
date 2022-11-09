a7fb10b5fc402a6306c9daa6617719fa287a297f
Suicide5quad <okayawesome121@gmail.com>
2022-11-09 10:08:52 +0530

    added cors

--- FidZulu Toys/ToysSite/app.js
+++ FidZulu Toys/ToysSite/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 // var usersRouter = require("./routes/users");
@@ -25,7 +26,7 @@ app.use("/", indexRouter);
 // app.use("/users", usersRouter);
 app.use("/toys", toysRouter);
 // app.use("/toysTeam", toysTeamRouter);
-
+app.use(cors);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
